66c4d530fb62b5fa828ef6dfd39fc8e242848c10
e.mishchenko <mitschenkoelena8@gmail.com>
2025-06-04 22:32:08 +0300

    added homework_26

---
+++ blogs/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Заголовок",
+        help_text="Введите заголовок",
+    )
+
+    content = models.TextField(
+        verbose_name="Содержание",
+        help_text="Введите содержание",
+    )
+
+    preview = models.ImageField(
+        upload_to="post/preview",
+        blank=True,
+        null=True,
+        verbose_name="Превью(изображение)",
+        help_text="Загрузите фото(изображение)",
+    )
+
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
+
+    publication_sign = models.BooleanField(
+        default = False,
+        verbose_name="Признак публикации",
+    )
+
+    views_count = models.PositiveIntegerField(
+        verbose_name="Счетчик просмотров",
+        help_text="Укажите количество просмотров",
+        default=0,
+    )
+
+    class Meta:
+        verbose_name = "запись"
+        verbose_name_plural = "записи"
+
+
+    def __str__(self):
+        return self.title
--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("catalog.urls", namespace="catalog")),
+    path("catalog/", include("catalog.urls", namespace="catalog")),
+    path("blogs/", include("blogs.urls", namespace="blogs")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
